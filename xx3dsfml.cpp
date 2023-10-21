8ba3c26fe661a8645115af5fb445465cf3e238a6
Catwashere <catwashere_1999@hotmail.com>
2023-10-21 00:46:41 -0600

    feature(validations): replicate windows app validations (#4)
    * feature(validations): replicate windows app validations
    
    * feature(validations): replicate windows app validations
    
    ---------
    
    Co-authored-by: Gerardo Ezquerra <gerardo.ezquerra@yalo.com>

--- xx3dsfml.cpp
+++ xx3dsfml.cpp
@@ -3,6 +3,7 @@
  * Copyright 2023 Chris Malnick. All rights reserved.
  */
 
+
 #include <libftd3xx/ftd3xx.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
@@ -55,9 +56,14 @@
 #define FRAMERATE 60
 
 FT_HANDLE handle;
+
+ULONG device_id;
+char * device_description = new char[32];
+char * device_serial_number = new char[16];
+
+uint8_t connected_3ds = 0;
 bool connected = false;
 bool running = true;
-bool already_ask_audio = false;
 bool disconnect_and_connect = false;
 
 int curr_buf = 0;
@@ -71,6 +77,7 @@ public:
 	N3DSAudio(){
 		initialize(AUDIO_CHANNELS, AUDIO_SAMPLE_RATE);
 		setProcessingInterval(sf::milliseconds(0));
+		setVolume(20);
 	} 
 
 	void queue(IntVector samples){
@@ -98,7 +105,6 @@ private:
 
 		m_bmux.unlock();
 
-
 		size_t bfs=bu.size();
 		sf::Int16* bf=new sf::Int16[bfs]; 
 
@@ -117,67 +123,59 @@ private:
 	}
 };
 
-bool handle_open(){
-	for (int i=0; i < NUM_PRODUCTS; i++) {
-		char* product = const_cast<char*>(PRODUCTS[i]);
-		if (FT_Create(product, FT_OPEN_BY_DESCRIPTION, &handle) == FT_OK) {
-			printf("[%s] Create for %s successed.\n", NAME, PRODUCTS[i]);
-			return true;
-		}
-	}
-	return false;
-}
-
-bool ask_for_audio(){
-	if(already_ask_audio){
+bool handle_open(int8_t idx){
+	if (FT_SUCCESS(FT_Create(device_description, FT_OPEN_BY_DESCRIPTION, &handle))) {
+		printf("[%s] Create succeeded.\n", NAME);
 		return true;
 	}
+	return false;
+};
 
+int32_t ask_for_audio(){
 	UCHAR buf[4] = {0x40, 0x80, 0x00, 0x00};
 	ULONG written = 0;
 
-	if (FT_WritePipe(handle, BULK_OUT, buf, 4, &written, 0)) {
-		FT_AbortPipe(handle, BULK_IN);
+	if (FT_FAILED(FT_WritePipe(handle, BULK_OUT, buf, 4, &written, 0))) {
+		FT_AbortPipe(handle, BULK_OUT);
 		printf("[%s] Write 1 failed.\n", NAME);
-		return false;
+		return 0;
 	}
 	
-	if (FT_AbortPipe(handle, BULK_OUT)) {
+	if (FT_FAILED(FT_AbortPipe(handle, BULK_IN))) {
 		printf("[%s] Abort failed.\n", NAME);
-		return false;
+		return 0;
 	}
 	
-	UCHAR buf2[4] = {0x05, 0x98, 0x00, 0x00};
+	UCHAR buf2[16] = {0x98, 0x05, 0x9f, 0x0};
 	ULONG returned = 0;
-
-	if (FT_WritePipe(handle, BULK_OUT, buf2, 4, &returned, 0)) {
-		FT_AbortPipe(handle, BULK_IN);
+	
+	if (FT_FAILED(FT_WritePipe(handle, BULK_OUT, buf2, 4, &returned, 0))) {
+		FT_AbortPipe(handle, BULK_OUT);
 		printf("[%s] Write 2 failed.\n", NAME);
-		return false;
+		return 0;
 	}
 
-	if (FT_ReadPipe(handle, BULK_IN, buf2, 4, &returned, 0)) {
+	if (FT_FAILED(FT_ReadPipe(handle, BULK_IN, buf2, 16, &returned, 0))) {
 		FT_AbortPipe(handle, BULK_IN);
 		printf("[%s] Read failed.\n", NAME);
-		return false;
+		return 0;
 	}
-
-	already_ask_audio = true;
-
-	return true;
+	
+	uint32_t bsId = (((((buf2[4] << 8) | buf2[3]) << 8) | buf2[2]) << 8) | buf2[1];
+	return (bsId & 0xf0f0ff) == 0xc0b0a1 ? bsId : 0;
 }
 
 bool initialize(){
 	UCHAR buf[4] = {0x40, 0x00, 0x00, 0x00};
 	ULONG written = 0;
 
-	if (FT_WritePipe(handle, BULK_OUT, buf, 4, &written, 0)) {
-		FT_AbortPipe(handle, BULK_IN);
-		printf("[%s] Write failed.\n", NAME);
+	if (FT_FAILED(FT_WritePipe(handle, BULK_OUT, buf, 4, &written, 0))) {
+		FT_AbortPipe(handle, BULK_OUT);
+		printf("[%s] Write 7 failed.\n", NAME);
 		return false;
 	}
 
-	if (FT_SetStreamPipe(handle, false, false, BULK_IN, BUF_SIZE)) {
+	if (FT_FAILED(FT_SetStreamPipe(handle, false, false, BULK_IN, BUF_SIZE))) {
 		printf("[%s] Stream failed.\n", NAME);
 		return false;
 	}
@@ -185,49 +183,202 @@ bool initialize(){
 	return true;
 }
 
-bool open() {
-	disconnect_and_connect = false;
+int8_t* listDevices(){
+	static int8_t info[3];
+	info[0] = 0;
+	FT_STATUS ftStatus;
+	DWORD numDevs = 0;
+	ftStatus = FT_CreateDeviceInfoList(&numDevs);
+	if (FT_SUCCESS(ftStatus)){
+		if(numDevs < connected_3ds){
+			disconnect_and_connect = false;
+		}
+		connected_3ds = numDevs;
 
-	if (connected || handle != NULL) {
+		if(numDevs > 0){
+			FT_DEVICE_LIST_INFO_NODE *devInfo = (FT_DEVICE_LIST_INFO_NODE*)malloc(
+			sizeof(FT_DEVICE_LIST_INFO_NODE) * numDevs);
+			ftStatus = FT_GetDeviceInfoList(devInfo, &numDevs);
+			if (FT_SUCCESS(ftStatus))
+			{
+				for (DWORD i = 0; i < numDevs; i++)
+				{
+					for (int8_t j=0; j < NUM_PRODUCTS; j++) {
+						const char* p = PRODUCTS[j];
+						if(strcmp(devInfo[i].Description, p) == 0){
+							info[0] = j+1; // Index on PRODUCTS + 1 of existance
+							info[1] = devInfo[i].Flags & FT_FLAGS_OPENED; // ISOPEN
+							info[2] = (devInfo[i].Flags & FT_FLAGS_SUPERSPEED) ? 3 : (devInfo[i].Flags & FT_FLAGS_HISPEED) ? 2 : 0; // USBVERSION
+							handle = devInfo[i].ftHandle; // HANDLE
+							memcpy (device_description, devInfo[i].Description, 32);
+							memcpy (device_serial_number, devInfo[i].SerialNumber, 16);
+							device_id = devInfo[i].ID;
+							break;
+						}
+					}
+
+					if(info[0]){
+						break;
+					}
+				}
+			}
+			free(devInfo);
+		}
+	}
+	
+	return info;
+}
+
+uint32_t getFW(){
+	UCHAR buf[4] = {0x80, 0x01, 0xab, 0x00};
+	ULONG written = 0;
+
+	if (FT_FAILED(FT_WritePipe(handle, BULK_OUT, buf, 4, &written, 0))) {
+		FT_AbortPipe(handle, BULK_OUT);
+		printf("[%s] Write 3 failed.\n", NAME);
+		return -1;
+	}
+	
+	UCHAR buf2[8] = {0x90, 0x08, 0x03, 0x02, 0x00, 0x00, 0x00, 0x00};
+	ULONG returned = 0;
+	
+	if (FT_FAILED(FT_WritePipe(handle, BULK_OUT, buf2, 8, &returned, 0))) {
+		FT_AbortPipe(handle, BULK_OUT);
+		printf("[%s] Write 4 failed.\n", NAME);
+		return -1;
+	}
+	
+	if (FT_FAILED(FT_ReadPipe(handle, BULK_IN, buf2, 8, &returned, 0))) {
+		FT_AbortPipe(handle, BULK_IN);
+		printf("[%s] Read failed.\n", NAME);
+		return -1;
+	}
+
+	uint32_t fwId = (((((buf2[7] << 8) | buf2[6]) << 8) | buf2[5]) << 8) | buf2[4];
+	return (fwId & 0xf0feff) == 0xc0b0a1 ? (fwId >> 24) : 0;
+}
+
+uint32_t reset(){
+	UCHAR buf[4] = {0x80, 0x01, 0xab, 0x00};
+	ULONG written = 0;
+	
+	if (FT_FAILED(FT_WritePipe(handle, BULK_OUT, buf, 4, &written, 0))) {
+		FT_AbortPipe(handle, BULK_OUT);
+		printf("[%s] Write 5 failed.\n", NAME);
+		return 0;
+	}
+	
+	UCHAR buf2[4] = {0x43, 0x00, 0x00, 0x00};
+	ULONG returned = 0;
+	
+	if (FT_FAILED(FT_WritePipe(handle, BULK_OUT, buf2, 4, &returned, 0))) {
+		FT_AbortPipe(handle, BULK_OUT);
+		printf("[%s] Write 6 failed.\n", NAME);
+		return 0;
+	}
+	
+	size_t i = 0;
+	int32_t bsId;
+	do
+	{
+		usleep(200);
+		bsId = ask_for_audio();
+		if (bsId != 0)
+        {
+			return (bsId & 0xF00) == 0x100 ? bsId : 0;
+		}
+		i++;
+	} while (i < 3);
+	return bsId;
+}
+
+bool close() {
+	disconnect_and_connect = true;
+
+	if (FT_Close(handle)) {
+		printf("[%s] Close failed.\n", NAME);
 		return false;
 	}
 
-	if (!handle_open()) {
+	printf("[%s] Connection closed.\n", NAME);
+	connected = false;
+	return true;
+}
+
+bool open() {
+	if (connected || disconnect_and_connect) {
+		return false;
+	}
+
+	int8_t *info = listDevices();
+	if(!info[0]){
+		return false;
+	}
+
+	if (!handle_open(info[1])) {
 		printf("[%s] Create failed.\n", NAME);
+		disconnect_and_connect = true;
 		return false;
 	}
 
 	if(FT_SetPipeTimeout(handle, BULK_OUT, 200)){
 		printf("[%s] Timeout failed.\n", NAME);
+		close();
 		return false;
 	}
 
 	if(FT_SetPipeTimeout(handle, BULK_IN, 80)){
 		printf("[%s] Timeout failed.\n", NAME);
+		close();
 		return false;
 	}
 
 	if(FT_ClearStreamPipe(handle, true, true, 0)){
 		printf("[%s] Clear failed.\n", NAME);
-	}
-
-	if (!ask_for_audio()) {
-		printf("[%s] Ask for audio failed.\n", NAME);
-
-		if (FT_Close(handle)) {
-			printf("[%s] Close failed.\n", NAME);
-		}
-
-		disconnect_and_connect = true;
-		handle = NULL;
-
+		close();
 		return false;
 	}
-	
+
+	int32_t bsId = ask_for_audio();
+	if (!bsId) {
+		printf("[%s] Ask for audio failed.\n", NAME);
+		close();
+		return false;
+	}
+
+	int32_t parse = (bsId & 0xf00);
+	uint32_t fw;
+	if(parse != 0x0){
+		fw = (bsId >> 24);
+	}else{
+		fw = getFW();
+	}
+
+	if(parse == 0x0 && fw < 0){
+		printf("[%s] Get FW failed.\n", NAME);
+		close();
+		return false;
+	}
+
+	printf("[%s] %s, SN=%s, FW=%d, USB%d\n", NAME, device_description, device_serial_number, fw, info[2]);
+
+	if(parse == 0x0){
+		bsId = reset();
+		parse = (bsId & 0xf00);
+		fw = -1;
+		if(parse != 0x0){
+			fw = (bsId >> 24);
+		}
+	}
+	if(!bsId || fw < 0){
+		printf("[%s] Get FW after reset failed.\n", NAME);
+		close();
+		return false;
+	}
+
 	if(!initialize()){
 		printf("[%s] Initialize failed.\n", NAME);
-		
-
+		close();
 		return false;
 	}
 
@@ -239,16 +390,27 @@ void capture() {
 	OVERLAPPED overlap[BUF_COUNT];
 
 start:
+	uint64_t i = 0;
 	while (!connected) {
-		if(disconnect_and_connect){
-			printf("[%s] please disconnect and connect cable, then press 1.\n", NAME);
-		}else{
-			printf("[%s] waiting.\n", NAME);
-		}
-
 		if (!running) {
 			return;
 		}
+
+		int8_t *info = listDevices();
+		if(i){
+			printf("\x1b[A");
+		}
+		i++;
+		if(disconnect_and_connect){
+			printf("[%s] please disconnect and connect cable.\n", NAME);
+		}else{
+			printf("[%s] please connect 3ds                 .\n", NAME);
+
+			if(info[0]){
+				connected = open();
+			}
+		}
+
 		sleep(1);
 	}
 	
@@ -288,23 +450,19 @@ start:
 
 end:
 	for (curr_buf = 0; curr_buf < BUF_COUNT; ++curr_buf) {
-		if (FT_ReleaseOverlapped(handle, &overlap[curr_buf])) {
+		if (FT_FAILED(FT_ReleaseOverlapped(handle, &overlap[curr_buf]))) {
 			printf("[%s] Release failed.\n", NAME);
 		}
 	}
 
-	if(FT_ClearStreamPipe(handle, false, false, BULK_IN)){
-		printf("[%s] Clear failed.\n", NAME);
-	}
+	FT_AbortPipe(handle, BULK_OUT);
 
-	if (FT_Close(handle)) {
+	if (FT_FAILED(FT_Close(handle))) {
 		printf("[%s] Close failed.\n", NAME);
 	}
 
 	printf("[%s] Connection closed.\n", NAME);
 	connected = false;
-	sleep(5); // need to find a better way to avoid segmantation fault when multiple on/off
-	handle = NULL;
 
 	goto start;
 }
@@ -466,7 +624,7 @@ change:
 				case sf::Event::KeyPressed:
 					switch (event.key.code) {
 					case sf::Keyboard::Num1:
-						connected = open();
+						//connected = open();
 						break;
 
 					case sf::Keyboard::Num2:
@@ -549,10 +707,10 @@ change:
 
 					case sf::Keyboard::Num0:
 						win[0]->close();
-						free(win[0]);
+						delete win[0];
 						if(windows == 2){
 							win[1]->close();
-							free(win[1]);
+							delete win[1];
 						}
 						windows = (windows == 2 ? 1 : 2);
 						goto change;
@@ -608,13 +766,20 @@ change:
 		}
 	}
 	
+	if(windows == 2){
+		win[1]->close();
+		delete win[1];
+	}
+	delete win[0];
+
+	audioStream->stop();
+	delete audioStream;
+
 	running = false;
 	thread.join();
 }
 
 int main() {
-	connected = open();
 	render();
-
 	return 0;
 }
